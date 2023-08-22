d3b84e559c092ecda0553e8f41034e07221c69f3
naveen mutham <107289536+nmutham@users.noreply.github.com>
2023-08-22 11:17:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry oatmeal')
+streamlit.text('🥣 Omega 3 & Bluberry oatmeal')
