f137a9ecf6e1b66b5cddf93336c118190d06d28f
naveen mutham <107289536+nmutham@users.noreply.github.com>
2023-08-22 11:19:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Bluberry oatmeal')
+streamlit.text(' 🥗  Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
