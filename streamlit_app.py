f413d319de16e6d67b9d57d585b16ee1b252011a
naveen mutham <107289536+nmutham@users.noreply.github.com>
2023-08-24 16:47:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import snowflake.connector
 import streamlit
+import pandas
+import requests
+from urllib.error import URLError
+
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Bluberry oatmeal')
@@ -8,7 +12,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Choose the Fruit Name Column as the Index
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -26,14 +30,17 @@ streamlit.dataframe(fruits_to_show)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_repsonse = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
 # take the json version and normalize it
 fruityvice_repsonse_normalized = pandas.json_normalize(fruityvice_repsonse.json())
 # output it on the screen as a table
 streamlit.dataframe(fruityvice_repsonse_normalized)
 
-#Let's Query Our Trial Account Metadata 
+#don't run anything past here till we troubleshoot
+streamlit.stop()
+
+#Let's connect to snowflake and get a list of fruits from the table in snowflake 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
