638d5dbec13cceff11bb8bcf6c0b65283e3ab0b2
naveen mutham <107289536+nmutham@users.noreply.github.com>
2023-08-24 16:11:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,11 @@ fruityvice_repsonse = requests.get("https://www.fruityvice.com/api/fruit/" + fru
 fruityvice_repsonse_normalized = pandas.json_normalize(fruityvice_repsonse.json())
 # output it on the screen as a table
 streamlit.dataframe(fruityvice_repsonse_normalized)
+
+#Let's Query Our Trial Account Metadata 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
